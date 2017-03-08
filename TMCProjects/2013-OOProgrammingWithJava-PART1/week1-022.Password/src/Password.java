33c57d49e82147da65b0ed915f62b2c34d5441fb
Grave Rider <donprayer@gmail.com>
2017-03-09 01:41:12 +0200

    Excercise 022

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week1-022.Password/src/Password.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week1-022.Password/src/Password.java
@@ -5,8 +5,18 @@ public class Password {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-        String password = "carrot"; // Use carrot as password when running tests.
 
-        // Write your code here
+        while (true) {
+            System.out.println("Type the password: ");
+            String password = reader.nextLine();
+
+            if (password.equals("carrot")) {
+                System.out.println("Right!" + "\n" +
+                        "The secret is jryy qbar!");
+                break;
+            } else {
+                System.out.println("Wrong!");
+            }
+        }
     }
 }
