60ed1b6de8a8b266427f4ac261075021534bc8c0
Grave Rider <donprayer@gmail.com>
2017-03-09 00:15:33 +0200

    Excercise 013

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week1-013.NhlStatisticsPart1/src/Main.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week1-013.NhlStatisticsPart1/src/Main.java
@@ -1,8 +1,27 @@
 
 import nhlstats.NHLStatistics;
 
+
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("Top ten by goals");
+        NHLStatistics.sortByGoals();
+        NHLStatistics.top(10);
+
+        System.out.println("Top twenty-five by penalty");
+        NHLStatistics.sortByPenalties();
+        NHLStatistics.top(25);
+
+        System.out.println("Sidney Crosby stats");
+        NHLStatistics.searchByPlayer("Sidney Crosby");
+
+        System.out.println("Philadelphia Flyers stats");
+        NHLStatistics.teamStatistics("PHI");
+
+        System.out.println("Players of Anaheim Ducks by points");
+        NHLStatistics.sortByPoints();
+        NHLStatistics.teamStatistics("ANA");
     }
 }
