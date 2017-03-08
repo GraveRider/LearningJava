52679337a8fee88e6733f5a09d0c1a053cd63bd6
Grave Rider <donprayer@gmail.com>
2017-03-09 00:57:03 +0200

    Excercise 017

--- TMCProjects/2013-OOProgrammingWithJava-PART1/week1-017.GreaterNumber/src/GreaterNumber.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week1-017.GreaterNumber/src/GreaterNumber.java
@@ -5,5 +5,20 @@ public class GreaterNumber {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
+        System.out.println("Type the first number: ");
+        double num1 = Double.parseDouble(reader.nextLine());
+        System.out.println("Type the second number: ");
+        double num2 = Double.parseDouble(reader.nextLine());
+
+        if (num1 > num2) {
+            System.out.println("Greater number " + num1);
+        }
+        else if (num1 < num2) {
+            System.out.println("Greater number " + num2);
+        }
+        else {
+            System.out.println("The numbers are equal!");
+        }
+
     }
 }
