38d79e8af37cb5656df15e395e1fa065538af1b5
Grave Rider <donprayer@gmail.com>
2017-03-10 12:22:18 +0200

    Excercise 054

---
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week3-054.TheEndPart/src/TheEndPart.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TheEndPart {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Type a word: ");
+        String name = reader.nextLine();
+        System.out.println("Length of the end part: ");
+        int length = Integer.parseInt(reader.nextLine());
+
+        System.out.println("Result: " + name.substring(name.length() - length, name.length()));
+    }
+}
