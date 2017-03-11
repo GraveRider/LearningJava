b39a2716604a42b1def35dfdf069449518b195c6
Grave Rider <donprayer@gmail.com>
2017-03-11 20:45:23 +0200

    Excercise 073

---
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week4-073.Product/src/Product.java
@@ -0,0 +1,16 @@
+
+public class Product {
+
+    private double price;
+    private int amount;
+    private String name;
+
+    public Product(String nameAtStart, double priceAtStart, int amountAtStart) {
+        name = nameAtStart;
+        price =priceAtStart;
+        amount =amountAtStart;
+    }
+    public void printProduct() {
+        System.out.println(name + ", price " + price + ", amount " + amount);
+    }
+}
