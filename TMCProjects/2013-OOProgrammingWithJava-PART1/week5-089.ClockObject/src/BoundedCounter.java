840d9e85909871b3a969d5a03eb14cf384b0d542
Grave Rider <donprayer@gmail.com>
2017-04-13 15:40:50 +0300

    Excercise 089

---
+++ TMCProjects/2013-OOProgrammingWithJava-PART1/week5-089.ClockObject/src/BoundedCounter.java
@@ -0,0 +1,38 @@
+
+public class BoundedCounter {
+
+    private int value;
+    private int upperLimit;
+
+    public BoundedCounter(int upperLimit) {
+        value = 0;
+        this.upperLimit = upperLimit;
+    }
+
+    public void next() {
+        value++;
+
+        if (value > upperLimit) {
+            value = 0;
+        }
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        if (value >= 0 && value <= upperLimit) {
+            this.value = value;
+        }
+    }
+
+    public String toString() {
+        if (value < 10) {
+            return "0" + value;
+        }
+        else {
+            return "" + value;
+        }
+    }
+}
