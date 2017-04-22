80b854a086fc82f0674370b42297db5e7177a23d
Grave Rider <donprayer@gmail.com>
2017-04-22 21:39:12 +0300

    Excercise 06

--- TMCProjects/2013-OOProgrammingWithJava-PART2/week7-week7_06.PromissoryNote/src/Main.java
+++ TMCProjects/2013-OOProgrammingWithJava-PART2/week7-week7_06.PromissoryNote/src/Main.java
@@ -1,5 +1,12 @@
+
 public class Main {
     public static void main(String[] args) {
-        // Test your program here
+        PromissoryNote note = new PromissoryNote();
+
+        note.setLoan("Robert", 500.40);
+        note.setLoan("Robert", 350);
+        System.out.println("Total debt: " + note.howMuchIsTheDebt("Robert"));
+        System.out.println("Total debt: " + note.howMuchIsTheDebt("Alex"));
+
     }
 }
