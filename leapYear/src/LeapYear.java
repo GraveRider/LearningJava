7b37d9187df557a8b5d85f2158260e01084caad9
Grave Rider <donprayer@gmail.com>
2017-04-22 17:53:13 +0300

    Redefined method's name from leapYearCounter to calculateLeapYear

--- leapYear/src/LeapYear.java
+++ leapYear/src/LeapYear.java
@@ -7,23 +7,23 @@ public class LeapYear {
     private static int counter;
 
     public static void main(String[] args) {
-        leapYearCounter(2017);
+        calculateLeapYear(2017);
     }
 
     /**
      * Method which calculates and prints 20 results of leap years from given year.
      * @param year year from which calculate leap years.
      */
-    public static void leapYearCounter(int year) {
+    public static void calculateLeapYear(int year) {
 
         if (year % 4 == 0 && counter < 20) {
             System.out.println(year + " year is leap year");
             counter++;
             year++;
-            leapYearCounter(year);
+            calculateLeapYear(year);
         } else if(year % 4 != 0){
             year++;
-            leapYearCounter(year);
+            calculateLeapYear(year);
         }
     }
 }
